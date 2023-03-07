98f8368c3ce8a30acc1142ec2fb2ccf945f4192f
MatthewFoster02 <mattfoster02@outlook.com>
2023-03-07 23:16:42 +0100

    Added create/read functions
    Added the create and get User functions

--- src/backend/crud/users.py
+++ src/backend/crud/users.py
@@ -2,5 +2,9 @@ from fastapi import Body, Request
 
 from models.users import UserBase
 
-def createUser(request:Request, newUser:UserBase=Body(...)):
-    pass
+async def createUser(request:Request, newUser:UserBase=Body(...)):
+    user = await request.app.mongodb['users'].insert_one(newUser)
+    return await request.app.mongodb['users'].find_one({'_id': user.inserted_id})
+
+async def getUser(request:Request, userID:int):
+    user = request.app.mongodb['users'].find_one({'_id': userID})
