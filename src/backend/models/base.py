c72d427ede74769ce1427cac7715202b4e45ef2b
MatthewFoster02 <mattfoster02@outlook.com>
2023-03-07 22:31:21 +0100

    Created base and user model in /models
    Base model converts ObjectID used by mongoDB to string for use in python

---
+++ src/backend/models/base.py
@@ -0,0 +1,22 @@
+from bson import ObjectId
+from pydantic import BaseModel, Field
+
+class PyObjectID(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not(ObjectId.is_valid(v)):
+            raise ValueError('Invalid Object ID')
+        return ObjectId(v)
+    
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
+
+class MongoBaseModel(BaseModel):
+    id: PyObjectID = Field(default_factory=PyObjectID, alias='_id')
+    class Config:
+        json_encoders = {ObjectId: str}
---
+++ src/backend/models/users.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from email_validator import validate_email, EmailNotValidError
+from pydantic import BaseModel, EmailStr, Field, validator
+
+from models.base import MongoBaseModel
+
+class UserBase(MongoBaseModel):
+    firstname:str = Field(..., max_length=20)
+    lastname:str = Field(..., max_length=20)
+    username:str = Field(..., max_length=16)
+    email:str = Field(...)
+    password:str = Field(..., min_length=8, max_length=16)
+    profilePic:Optional[str] = None
+    bio:str = Field(..., max_length=200)
+    birthday:Optional[int] = None
+
+    @validator('email')
+    def valid_email(cls, v):
+        try:
+            email = validate_email(v).email
+            return email
+        except EmailNotValidError:
+            raise EmailNotValidError
+
+class LoginBase(BaseModel):
+    email:str = EmailStr(...)
+    password:str = Field(...)
