bde444db452245beb19e0ca57c53d574e738b753
MatthewFoster02 <mattfoster02@outlook.com>
2023-04-15 02:09:15 +0200

    Added functionality for switch between posting and asking GPT

--- src/frontend/src/components/HomeHeader.js
+++ src/frontend/src/components/HomeHeader.js
@@ -25,25 +25,60 @@ const HomeHeader = () =>
 
     const onErrors = (errors) => console.error(errors);
 
+    // GPT API stuff
+    const toggleGPT = () =>
+    {
+        const formPost = document.getElementById('post-form');
+        const formGPT = document.getElementById('gpt-form');
+        const switchBtn = document.getElementById('toggle');
+
+        formPost.classList.toggle('no-display');
+        formGPT.classList.toggle('no-display');
+
+        switchBtn.innerHTML === 'Assistant' ? switchBtn.innerHTML = 'Post' : switchBtn.innerHTML = 'Assistant';
+    }
+
     return (
         <div className="wrapperHH">
             <h2 className="titleLogin">
                 Home
             </h2>
             <div class="form">
-                <form onSubmit={handleSubmit(createPost, onErrors)}>
-                    <div className="make-post">
-                        <input
-                            type="text"
-                            placeholder="Who are you looking for?"
-                            className="add-post"
-                        />
-                        <button className="post">
+                <form onSubmit={handleSubmit(createPost, onErrors)} id="post-form">
+                    <div className="add-post">
+                        <textarea 
+                            placeholder="What are you going to post?"
+                            className="make-post"
+                            cols="70"
+                            rows="4"
+                            resize="none"
+                            name="post"
+                            id="post"
+                        >
+                        </textarea>
+                        <button className="btn-hh">
                             Post
                         </button>
                     </div>
                 </form>
-                <button className="gpt">
+                <form onSubmit={handleSubmit(createPost, onErrors)} id="gpt-form" className="no-display">
+                    <div className="add-post">
+                        <textarea 
+                            placeholder="Enter prompt for GPT"
+                            className="make-post"
+                            cols="70"
+                            rows="4"
+                            resize="none"
+                            name="post"
+                            id="post"
+                        >
+                        </textarea>
+                        <button className="btn-hh">
+                            Ask
+                        </button>
+                    </div>
+                </form>
+                <button className="btn-hh" onClick={toggleGPT} id="toggle">
                     Assistant
                 </button>
             </div>
