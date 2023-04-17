12ef61ac0e91d12ece3b8709cc629d47d0c8ff6c
MatthewFoster02 <mattfoster02@outlook.com>
2023-04-17 02:46:55 +0200

    Added pagination navigation to posts.

--- src/frontend/src/components/DisplayPosts.js
+++ src/frontend/src/components/DisplayPosts.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
 import postsAPI from '../apiHandlers/posts.js';
-import usersAPI from '../apiHandlers/users.js';
 import Post from "./Post.js";
 import './DisplayPosts.css';
 
@@ -31,9 +30,11 @@ const DisplayPosts = ({props}) =>
             if(res_posts['statusText'] === 'OK')
             {
                 const posts = await res_posts['data'];
+                console.log(posts);
                 setPosts(posts.posts.reverse());
-                setTotalPages(posts.totalPages);
+                setTotalPages(posts.total_pages);
                 setIsPending(false);
+                console.log('TOTAL PAGES: '+totalPages);
             }
         }
         fetchPosts();
@@ -69,6 +70,15 @@ const DisplayPosts = ({props}) =>
                 }
                 )
             }
+            <div className="page-chooser">
+                <div className="page-location">
+                    <h4>Showing page <span className="current-page">{page}</span> of <span className="total-pages">{totalPages}</span>.</h4>
+                </div>
+                <div className="page-navigation">
+                    <button className="previous-page" onClick={previousPage}>Previous Page</button>
+                    <button className="next-page" onClick={nextPage}>Next Page</button>
+                </div>
+            </div>
         </div>
     );
 }
