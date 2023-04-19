641003905086f84396e6e73dfdf1ad8b6e18618f
MatthewFoster02 <mattfoster02@outlook.com>
2023-04-19 14:00:55 +0200

    Like unliking wokring

--- src/frontend/src/components/Post.js
+++ src/frontend/src/components/Post.js
@@ -3,21 +3,36 @@ import { useState } from "react";
 
 import whiteHeart from '../images/like-white.png';
 import redHeartHollow from '../images/like-red-hollow.png';
+import redHeartFilled from '../images/like-red-filled.png';
 import comment from '../images/comment-white.png';
 import postsAPI from '../apiHandlers/posts.js';
 import './Post.css';
+import useAuth from "../hooks/useAuth.js";
 
 const Post = ({post}) => 
 {
     const { author, comments, content, date_posted, likes, _id, author_profile_pic, author_username } = post;
     const [likeVal, setLikeVal] = useState(likes);
+    const [postLiked, setPostLiked] = useState(false);
     const [heartImage, setHeartImage] = useState(whiteHeart);
+    const { auth, setAuth } = useAuth();
+
+    //if(auth.likers.contains(auth.id)) setPostLiked(true);
 
     const addLike = (e) =>
     {
         e.preventDefault();
         postsAPI.like(_id);
         setLikeVal(likeVal + 1);
+        setPostLiked(true);
+    }
+
+    const removeLike = (e) =>
+    {
+        e.preventDefault();
+        postsAPI.unlike(_id);
+        setLikeVal(likeVal - 1);
+        setPostLiked(false);
     }
 
     return (
@@ -30,20 +45,33 @@ const Post = ({post}) =>
                 {content}
             </p>
             <div className="statistics">
-                <div className="likes"> 
-                    <button className="like-btn" onClick={addLike}>
-                    <img
-                        src={heartImage}
-                        alt="Heart"
-                        width="50"
-                        height="50"
-                        className="like-img"
-                        onMouseOver={() => setHeartImage(redHeartHollow)}
-                        onMouseOut={() => setHeartImage(whiteHeart)}
-                    />
-                    </button>
-                    <p>{likeVal}</p>
-                </div>
+                {
+                    !postLiked ? <div className="likes"> 
+                        <button className="like-btn" onClick={addLike}>
+                            <img
+                                src={heartImage}
+                                alt="Heart"
+                                width="50"
+                                height="50"
+                                className="like-img"
+                                onMouseOver={() => setHeartImage(redHeartHollow)}
+                                onMouseOut={() => setHeartImage(whiteHeart)}
+                            />
+                        </button>
+                        <p>{likeVal}</p>
+                    </div> : <div className="likes"> 
+                        <button className="like-btn" onClick={removeLike}>
+                            <img
+                                src={redHeartFilled}
+                                alt="Heart"
+                                width="50"
+                                height="50"
+                                className="like-img"
+                            />
+                        </button>
+                        <p>{likeVal}</p>
+                    </div>
+                }
                 <div className="comments">
                     <img src={comment} className="comment-img" />
                     <p>{
